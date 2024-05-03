10a8f827b83e19d2ea3d8318bfe066a9f18390b3
prosperReigns <prosperkinging7@gmail.com>
2024-05-03 13:21:28 -0700

    change install flask code

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,6 @@
 #!/usr/bin/pup
 # install a specific version oflask (2.1.0)
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install flask==2.1.0',
-  path        => ['/usr/bin', '/usr/local/bin'],
-  environment => ['PATH=/usr/bin:/usr/local/bin'],
-  unless      => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+package {'flask':
+	ensure   => '2.1.0',
+	provider => 'pip'
 }
