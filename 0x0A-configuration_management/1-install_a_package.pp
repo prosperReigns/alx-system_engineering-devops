25f4c8d650325652a23f37c50b22d36458241e97
prosperReigns <prosperkinging7@gmail.com>
2024-05-03 12:57:33 -0700

    installing flask and killing a process

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,6 @@
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install flask==2.1.0',
+  path        => ['/usr/bin', '/usr/local/bin'],
+  environment => ['PATH=/usr/bin:/usr/local/bin'],
+  unless      => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+exec { 'kill_killmenow_process':
+  command     => 'pkill killmenow',
+  onlyif      => 'pgrep killmenow',
+  path        => ['/bin', '/usr/bin', '/usr/sbin'],
+  environment => ['PATH=/bin:/usr/bin:/usr/sbin'],
+}
